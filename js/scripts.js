82475a8771b6e62faccb679a82adcba32eeb2378
William Kulha <kulha.william@gmail.com>
2018-09-10 11:14:46 -0400

    Add functionality to all conversion panels and buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ var butter2grams = function(tablespoons) {
   return tablespoons * 14.18
 };
 
-var honeygram2cups = function(cups) {
+var honeycups2grams = function(cups) {
   return cups * 340
 };
 
@@ -30,17 +30,54 @@ var tempconversion = function(temp) {
   return (temp - 32) * (5/9)
 };
 
-$(document).ready(function(){
-  $("form#gallonsform").submit(function(event){
+$(document).ready(function() {
+  $("form#gallonsform").submit(function(event) {
     event.preventDefault();
-    var gallonsnum = parseInt($("#gallons").val());
+    var gallonsnum = parseInt($("input#gallons").val());
     var litersresult = gallons2Liters(gallonsnum);
     $("span.litersresult").text(litersresult + " liters");
   });
-  $("form#cupstoml").submit(function(event){
-    var cupsnum = parseInt($("#cups").val());
+  $("form#cupstoml").submit(function(event) {
+    event.preventDefault();
+    var cupsnum = parseInt($("input#cups").val());
     var milliletersresult = cups2Ml(cupsnum);
     $("span.milliletersresult").text(milliletersresult + " millileters")
+  });
+  $("form#flourcups").submit(function(event) {
+    event.preventDefault();
+    var cupsnum = parseInt($("input#cupsFlour").val());
+    var gramsresult = flour2Grams(cupsnum);
+    $("span.cupsFlourResult").text(gramsresult + " grams");
+  });
+  $("form#sugarcups").submit(function(event) {
+    event.preventDefault();
+    var cupsnum = parseInt($("input#cupsSugar").val());
+    var gramsresult = sugar2grams(cupsnum);
+    $("span.cupsSugarresult").text(gramsresult + "grams");
+  });
+  $("form#buttertbsp").submit(function(event) {
+    event.preventDefault();
+    var tbspnum = parseInt($("input#butter").val());
+    var gramsresult = butter2grams(tbspnum);
+    $("span.butterresult").text(gramsresult + " grams");
+  });
+  $("form#honeycups").submit(function(event) {
+    event.preventDefault();
+    var cupsnum = parseInt($("input#honeycups").val());
+    var gramsresult = honeycups2grams(cupsnum);
+    $("span.honeyresult").text(gramsresult + " grams");
+  });
+  $("form#chips").submit(function(event) {
+    event.preventDefault();
+    var cupsnum = parseInt($("input#chipscups").val());
+    var gramsresult = chocochips2cups(cupsnum);
+    $("span.chipsresult").text(gramsresult + " grams");
+  });
+  $("form#ovenTemp").submit(function(event){
+    event.preventDefault();
+    var fTemp = parseInt($("input#temp").val());
+    var cTemp = tempconversion(fTemp);
+    $(".tempresult").text(cTemp + " degrees Celsius");
   });
 
 });
